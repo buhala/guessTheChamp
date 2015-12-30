b4284ba25d29e55c5ed2cd449dd12f552335a8b2
buhala <fix288@gmail.com>
2015-12-30 11:29:49 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(function(){
+	$('#currentGuess').val('');
+    $('#currentGuess').prop('disabled',false);
+	$('#currentGuess').focus();
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+	function dumbDownName(name){
+		return name.toLowerCase().replace(" ",'').replace("'","");
+	}
+	$('#skip').click(function(){
+		pickNewChampion();
+		$('#currentGuess').focus();
+		
+	})
+	window.timerId=-1;
+	$.get("champion.json",function(data){
+		champs=data['data'];
+		window.champions=[];
+		count=0;
+		/*for(ch in window.champs){
+			window.champions[count++]=ch;
+		}*/
+		window.champions=_.map(champs,function(data){return data;})
+		pickNewChampion();
+	});
+	
+	function pickNewChampion(){
+		championChosen=Math.floor(Math.random()*_.size(window.champions));
+		window.currentChampion=window.champions[championChosen]; //black magics
+		$('#champion-title').html(capitalizeFirstLetter(window.currentChampion['title']));
+		$('#currentGuess').val('');
+	}
+$('#currentGuess').on('keyup',function(){
+	if(window.timerId==-1){
+		 window.timerId=setInterval(function(){
+			 $('#timer').html(parseInt($('#timer').html())-1);
+			 if($('#timer').html()==0){
+				 clearInterval(window.timerId);
+				 $('#currentGuess').prop('disabled',true);
+			 }
+	},1000);
+	}
+		 if(dumbDownName($('#currentGuess').val())==dumbDownName(window.currentChampion['name'])){
+			 pickNewChampion();
+			 
+			 $('#currentGuesses').html(parseInt($('#currentGuesses').html())+1);
+			 
+		 }
+		 console.log($('#currentGuess').val()+"=="+dumbDownName(window.currentChampion['name']));
+	
+	
+});
+});
